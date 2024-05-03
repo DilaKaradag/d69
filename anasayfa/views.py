d672e88761458d5757eedf7d8503c01645824642
Dila <165695398+DilaKaradag@users.noreply.github.com>
2024-05-03 10:38:00 +0000

    ..

--- anasayfa/views.py
+++ anasayfa/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def xxx(request):
+  return HttpResponse("Ana sayfaya hoş geldiniz.")
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import anasayfa.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('anasayfa/', admin.site.urls),
+    path('anasayfa/', views.xxx),
 ]
